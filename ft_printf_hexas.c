a54d04972c07a5f30d35a895c840a9e049e2edff
Luiza Kormann Carvalho Da Fonseca <lukorman@student.42sp.org.br>
2025-01-06 21:29:35 -0300

    revised code OK

--- ft_printf_hexas.c
+++ ft_printf_hexas.c
@@ -6,13 +6,13 @@
 /*   By: lukorman <lukorman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 02:07:54 by luiza             #+#    #+#             */
-/*   Updated: 2025/01/06 20:15:45 by lukorman         ###   ########.fr       */
+/*   Updated: 2025/01/06 21:09:03 by lukorman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_putpnt(void *ptr)
+int	ft_putptr(void *ptr)
 {
 	unsigned long	pnt;
 
@@ -23,18 +23,18 @@ int	ft_putpnt(void *ptr)
 	return (2 + puthexa(pnt, 0));
 }
 
-int	puthexa(unsigned long n, int uplow)
+int	puthexa(unsigned long n, int is_upper)
 {
 	int			count;
 	const char	*hex_chars;
 
 	count = 0;
-	if (uplow == 1)
+	if (is_upper == 1)
 		hex_chars = "0123456789ABCDEF";
 	else
 		hex_chars = "0123456789abcdef";
 	if (n >= 16)
-		count += puthexa(n / 16, uplow);
+		count += puthexa(n / 16, is_upper);
 	count += ft_putchar(hex_chars[n % 16]);
 	return (count);
 }
--- wraps_hexas.c
+++ wraps_hexas.c
@@ -6,7 +6,7 @@
 /*   By: lukorman <lukorman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 04:02:45 by luiza             #+#    #+#             */
-/*   Updated: 2025/01/06 20:19:25 by lukorman         ###   ########.fr       */
+/*   Updated: 2025/01/06 20:52:03 by lukorman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	wrap_ptr(va_list *args)
 {
-	return (ft_putpnt(va_arg(*args, void *)));
+	return (ft_putptr(va_arg(*args, void *)));
 }
 
 int	wrap_hex_low(va_list *args)
